71bc6af2e5ce394845b1f4bbf39aa96bfb69ad4b
nickthetj <nicholastjandra1@gmail.com>
2022-03-21 00:14:38 +0800

    player death logic

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 
 {
     // Start is called before the first frame update
     private Animator anim;
+    private Rigidbody2D rb;
+
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
@@ -21,6 +25,11 @@ public class PlayerLife : MonoBehaviour
     }
 
     private void Death() {
+        rb.bodyType =  RigidbodyType2D.Static;
         anim.SetTrigger("death");
+    }
+
+    private void RestartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
